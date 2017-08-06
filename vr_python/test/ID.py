f23a424d252503f6864d2202ecf8f8810e6cf053
Jangwon <wkddnjset@naver.com>
2017-08-06 12:16:47 +0900

    no message

---
+++ vr_python/test/ID.py
@@ -0,0 +1,15 @@
+'''
+AIBRIL 아이디
+
+ID : aibriltest028@yopmail.com
+password : aibril12#$
+
+
+test_Conversation Service Key
+
+"url" : "https://gateway.aibril-watson.kr/conversation/api",
+"username" : "3261ef29-4d9e-4220-ac52-72728ec18d68",
+"password" : "xz5IcLm6yDr1"
+
+'''
+
