1385b110e99a741119cd737f64dedce2d94efa3d
Jangwon <wkddnjset@naver.com>
2017-08-07 22:30:40 +0900

    자바 웹 크롤링

--- Java_Web_Crawling/src/main/java/jsoup/Naver_Search.java
+++ Java_Web_Crawling/src/main/java/jsoup/Naver_Search.java
@@ -15,9 +15,9 @@ public class Naver_Search {
 		System.out.print("지역을 입력하세요 : ");
 		word = wordReader.nextLine();
 		
-		Document doc = Jsoup.connect("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="+ word+"+맛집").get();
+		Document doc = Jsoup.connect("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="+word+"+맛집").get();
 		Elements titles = doc.select("#sp_local_1 .info_area a");
-		String title = titles.attr("title");
+		String title = titles.attr("href");
 		Elements locations = doc.select("#sp_local_1 .ad_txt:eq(1)");
 		String location = locations.attr("title");
 		
